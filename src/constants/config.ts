fb156ae4f0474baef69fadf0521aeb933a08079e
Dmytro Kuzmin <crazyrider@ukr.net>
2023-03-27 14:36:28 +0300

    added phone number

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
 export enum Contacts {
-  phone = '+380667923483',
+  phone = '+380980523614',
   email = 'test@gmail.com',
   instagram = 'https://www.instagram.com/',
   facebook = 'https://www.facebook.com/',
